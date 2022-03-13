b2ef05daf120aacf34d210e5cdae89cbc4b4f936
donutz10 <rvrt2011@gmail.com>
2022-03-13 12:07:01 -0700

    added function to replace label when choice is selected

--- lab5.py
+++ lab5.py
@@ -3,32 +3,35 @@ from tkinter import ttk
 import tkinter as tk
 
 root = tk.Tk()
-root.geometry("350x350")
+root.geometry("150x150")
 r = IntVar()
 root.title('Color changer')
-
-
+Radiobutton(root, text="Red", variable = r, value = 1,command=lambda: color(r.get())).pack()
+Radiobutton(root, text="Blue", variable = r, value = 2, command= lambda: color(r.get())).pack()
+Radiobutton(root, text="green", variable = r, value = 3, command=lambda: color(r.get())).pack()
+myLabel = Label(root, text="No selection provided", bg='purple')
+myLabel.pack()
 def color(value):
     if value == 1:
+        global myLabel
+        myLabel.destroy()
         myLabel = Label(root, text="You have selected Red", bg="red", fg="white")
         myLabel.pack()
     elif value == 2:
+        myLabel.destroy()
         myLabel = Label(root, text="You have selected Blue", bg="Blue", fg="white")
         myLabel.pack()
     elif value == 3:
+        myLabel.destroy()
         myLabel = Label(root, text="You have selected Green", bg="green", fg="white")
         myLabel.pack()
         
 
 
 
-Radiobutton(root, text="Red", variable = r, value = 1,command=lambda: color(r.get())).pack()
-Radiobutton(root, text="Blue", variable = r, value = 2, command= lambda: color(r.get())).pack()
-Radiobutton(root, text="green", variable = r, value = 3, command=lambda: color(r.get())).pack()
 
 
-myLabel = Label(root, text="No selection provided", bg='purple')
-myLabel.pack()
+
 
 
 mainloop()
